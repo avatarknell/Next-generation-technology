d8da2b8d083d06d97585cd9b0c71bb618dc754e4
Hillary Chemiron <avatarknell@gmail.com>
2024-10-17 22:33:57 +0300

    responsive on tablet

--- Ecommerce/scripts.js
+++ Ecommerce/scripts.js
@@ -1,1 +1,12 @@
-const bar=document.getElementById
+
+const bar=document.getElementById("bar");
+const nav=document.getElementById("nav__mobile");
+const close=document.getElementById("close");
+
+bar.addEventListener('click', ()=>{
+    nav.classList.add("active");
+});
+
+close.addEventListener('click', ()=>{
+    nav.classList.remove("active");
+});
