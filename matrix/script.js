2deebaf39511bd9ecca013dc815ebd6effe56590
Hillary Chemiron <avatarknell@gmail.com>
2024-10-22 12:15:23 +0300

    inner HTML update

--- matrix/script.js
+++ matrix/script.js
@@ -1,2 +1,37 @@
-let cardTitle =document.getElementsByClassName("name")
-cardTitle.h2.innerHTML="MYron"
+const nameBtn = document.getElementById('nameBtn');
+
+let avatarName = document.getElementById('avatarName');
+let newAvatarName = document.getElementById("newAvatarName").value;
+
+let level=document.getElementById('level'); 
+let newLevel=document.getElementById('avatarLevel').value;
+
+let attribute=document.getElementById('attribute'); 
+let newAttribute=document.getElementById('avatarAttribute').value;
+
+let avatarClass=document.getElementById('class'); 
+let newCLass=document.getElementById('avatarClass').value; 
+
+let ability=document.getElementById('ability'); 
+let newAbility=document.getElementById('avatarAbility').value;
+
+let position=document.getElementById('position'); 
+let newPosition=document.getElementById('avatarPosition').value;
+
+let points=document.getElementById('points'); 
+let newPoints=document.getElementById('avatarPoints').value;
+
+const attributes=["Earth","Water","Fire", "Air", "Neutral", "lightning"]
+
+function newCard(){
+    avatarName.innerHTML=newAvatarName;
+    level.innerHTML=newLevel;
+    avatarClass.innerHTML=newCLass;
+    ability.innerHTML=newAbility;
+    points.innerHTML=newPoints;    
+    attribute.getElementsByTagName('img').className=newAttribute
+    } 
+
+
+nameBtn.addEventListener('click',newCard);
+
